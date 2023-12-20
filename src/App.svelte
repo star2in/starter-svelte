ecdf97d1a26aaae369aca16169ccc265a4714e24
John <43353250+star2in@users.noreply.github.com>
2023-12-20 01:07:31 -0500

    directus integration t1

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script>
 	export let name;
+	/** @type {import('./$types').PageData} */
+	export let data;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<h1>{data.Beds.Name}</h1>
+	<p>Length: {data.Beds.Length}, Width: {data.Beds.Length}</p>
 </main>
 
 <style>
